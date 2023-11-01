de4d8d567060605a0e1955fda9e1e5b6c5b04433
Wayan_Bharata <bharatayasa40@gmail.com>
2023-11-01 12:57:01 +0800

    update database

--- tomatify.sql
+++ tomatify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 27 Okt 2023 pada 21.27
+-- Waktu pembuatan: 01 Nov 2023 pada 12.53
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -24,18 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `savepredict`
+--
+
+CREATE TABLE `savepredict` (
+  `id` int(11) NOT NULL,
+  `kelas` varchar(20) NOT NULL,
+  `confidence` decimal(5,2) NOT NULL,
+  `description` text NOT NULL,
+  `prevention` text NOT NULL,
+  `userId` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `savepredict`
+--
+
+INSERT INTO `savepredict` (`id`, `kelas`, `confidence`, `description`, `prevention`, `userId`, `created_at`) VALUES
+(58, 'late blight', 3.05, 'Penyakit late blight, juga dikenal sebagai penyakit busuk daun, merupakan salah satu penyakit yang paling merusak pada tanaman tomat yang disebabkan oleh agen patogen bernama Phytophthora infestans (Mont.)', 'Pengendalian penyakit busuk daun dalam konteks budidaya tomat melibatkan sejumlah praktik pertanian dan tindakan pengendalian. Hal ini mencakup implementasi rotasi tanaman dan masa bera (periode tanah tidak ditanami), eliminasi tanaman tomat yang telah terinfeksi penyakit busuk daun, dan penghindaran produk tomat yang telah terkontaminasi. Selain itu, aplikasi fungisida juga merupakan metode umum untuk mengurangi perkembangan penyakit ini', 80, '2023-11-01 04:53:24');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `role` enum('admin','user') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `email` varchar(255) NOT NULL
+  `email` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -43,23 +66,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `name`, `password`, `role`, `created_at`, `updated_at`, `email`) VALUES
-(5, 'admin', 'Bharata', '$2b$10$pBM.DNc8cxm7YpFFPGaMYeWKSGQ12tFUJrA/J38bMMteCuaco87nO', 'admin', '2023-10-17 14:30:54', '2023-10-18 10:22:25', 'user1@ccc.com'),
-(20, '', '', '$2b$10$8cmXXovERt8Q1uUxQo91uurzmbKYSP3/INUAakHjpcouFejdS60Fa', 'user', '2023-10-19 03:51:47', '2023-10-19 03:51:47', ''),
-(23, 'fe8', 'fe', '$2b$10$23h4kW./P2q36XyUlP1SFuKDER0KuPbTNBKq0/C9K6ib/rEZumI6S', 'user', '2023-10-19 04:06:58', '2023-10-19 04:06:58', 'fe8@cc.com'),
-(25, 'fe10', 'fe10', '$2b$10$svKmKEZ24SWzc8n4.6rFqO7uzttDZUgoYhNP0SVk9CzGtIB239nGy', 'user', '2023-10-19 04:10:15', '2023-10-19 04:10:15', 'aa@gg.c'),
-(27, 'aa', 'aa12', '$2b$10$RMn.L9PsBgLEtZ79YQxHR.aHDQhpEZLBp60DHlBx4pH5/0EzQ6eOO', 'user', '2023-10-19 04:12:31', '2023-10-19 04:12:31', 'aa@bb.com'),
-(28, 'aa', 'aa12', '$2b$10$EUXLizu47wS5zbqmodOVWu/Gp.a6480XU3hVhO86Rr9wjKjcYQaeq', 'user', '2023-10-19 04:12:41', '2023-10-19 04:12:41', 'aa@bb.ccccc'),
-(39, 'aa', 'aa', '$2b$10$vPxt3xbstfYpFrrLuVOkgeeLbRjE9m2RdpS/1bo0H/cnpCY6kbo7K', 'admin', '2023-10-26 09:38:14', '2023-10-26 09:38:14', 'aa@ss.cc'),
-(40, 'user', 'user', '$2b$10$5BcQRK9s1cRUmXoPiNDe2uKksHiCCLF6wtYu6m3i74/IzmSnUa7d.', 'user', '2023-10-26 12:33:09', '2023-10-26 12:33:09', 'aa@ww.xx'),
-(42, 'dariadmin', 'dari admin', '$2b$10$MGMguSixs/ui7ImrGtzxRug9ZSEPx0II3xQn5sBYJhXdTHK0St7FW', 'user', '2023-10-26 15:37:59', '2023-10-26 15:37:59', 'min@ss.cc'),
-(43, 'modal', 'modal', '$2b$10$827/dn3yoHWewvo1rlAZ5.rl3io2Kl/f8D95EP2jun65yM/2YxcJW', 'user', '2023-10-27 02:04:57', '2023-10-27 02:04:57', 'modal@ss.cc'),
-(44, 'darimodal ', 'modal', '$2b$10$Bi2UI6kyPqWsTGMokohhEeC381MpXgV1ZK8et3ldj8QlVTUTx9BzG', 'user', '2023-10-27 02:28:02', '2023-10-27 02:28:02', 'modal@dd.cc'),
-(46, 'refresh', 'refresh', '$2b$10$aX9mWgnTIFuv71XvmA./AOgNCuKoxsbH2/gj5gRL6LM4IrMvRXj7e', 'user', '2023-10-27 04:03:31', '2023-10-27 04:03:31', 'refresh@ss.cc'),
-(48, 'modal', 'modal', '$2b$10$ShiySmwvdpSWUjigTmrTAOA3ByDKQPT43r3jOwrkSWLW8fH.OxQ92', 'user', '2023-10-27 10:39:01', '2023-10-27 10:39:01', 'modal@cc.dd');
+(51, 'admin', 'admin', '$2b$10$OnzXGDAA3UqJCnb8/qWIa.YbM0bkkUScZVuIW1NoM1ALa8UKVZydO', 'admin', '2023-10-28 04:58:06', '2023-10-29 16:53:42', 'admin@ss.xx'),
+(60, 'user', 'user', '$2b$10$ZT4Eyc0rQspNL/PBgJjU9u1amZU7FmkbKoA0TUse0q9ZW/OXjdEvW', 'user', '2023-10-29 15:51:09', '2023-10-29 15:51:09', 'user@cc.zz'),
+(61, 'wayan', 'wayan', '$2b$10$lpcdBzk2JEVbM.QwcGBOHuydHECHBEmNBRLUFulCnc.6KJwG1HpKG', 'user', '2023-10-30 02:27:00', '2023-10-30 02:27:00', 'wayan@gmail.com'),
+(63, 'nyoman', 'nyoman', '$2b$10$U0AMdW2n.2FXuGhZ/rvzoeeg.aig5DvEzwfT3Gma9uOKYVzX8z/my', 'user', '2023-10-30 02:27:40', '2023-10-30 02:29:16', 'nyoman@gmail.com'),
+(64, 'putu', 'putu', '$2b$10$hgxFqjBgFZJipTEPQ6JQEeoPGucfIkXDXmIOtmjSelEifqBbbwW4K', 'user', '2023-10-30 02:28:03', '2023-10-30 02:33:18', 'putu@gmail.com'),
+(65, 'gede', 'gede', '$2b$10$.CEQaSs0Bni3KYeTDOyiXu7isokkhY.VLsR9L2MlASw344oawRXDO', 'user', '2023-10-30 10:36:53', '2023-10-30 10:37:51', 'gede@yahoo.com'),
+(66, 'kadek', 'kadek', '$2b$10$A1hLEAY4rdRWC0Y2/uqQSuOeEjzPK8o4CQKIgZAnthT6mJpl6uQj6', 'user', '2023-10-30 10:42:05', '2023-10-30 10:42:05', 'kadek@gmail.com'),
+(67, 'komang', 'komang', '$2b$10$x4Wm4TyLdYz7w/9TDZFKw.PpgLvdoFTBnS3eneL5FXjZi7TGdW0xm', 'user', '2023-10-30 10:44:52', '2023-10-30 10:44:52', 'komang@gmail.com'),
+(80, 'tespredik', 'tes predik', '$2b$10$/zG3HcozEsbXabZQK2XLTeX2YO8P8NJ7n4LaYalYGsvOov/6WPQXe', 'user', '2023-11-01 04:51:45', '2023-11-01 04:51:45', 'tes@ss.ff');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `savepredict`
+--
+ALTER TABLE `savepredict`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
 
 --
 -- Indeks untuk tabel `users`
@@ -73,10 +99,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `savepredict`
+--
+ALTER TABLE `savepredict`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `savepredict`
+--
+ALTER TABLE `savepredict`
+  ADD CONSTRAINT `fk_savepredict_user` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
