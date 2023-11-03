2648d6670316cc1fa5b3900316973d46528e01c9
Wayan_Bharata <bharatayasa40@gmail.com>
2023-11-03 17:51:19 +0800

    modifikasi db.js

--- db.js
+++ db.js
@@ -3,29 +3,34 @@ require('dotenv').config();
 
 function createConnection() {
     const conn = mysql2.createConnection({
-        host     : process.env.DB_HOST,
-        user     : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_DATABASE
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     });
 
-    conn.connect((err) => {
-        if (err) {
-            console.error('Kesalahan saat membuat koneksi:', err);
-            setTimeout(createConnection, 2000);
-        } else {
-            console.log('nyambung ke database');
-        }
-    });
+    function handleDisconnect() {
+        conn.connect((err) => {
+            if (err) {
+                console.error('Kesalahan saat membuat koneksi:', err);
+                setTimeout(handleDisconnect, 2000);
+            } else {
+                console.log('Terhubung ke database');
+            }
+        });
 
-    conn.on('error', (err) => {
-        console.error('Kesalahan koneksi:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            setTimeout(createConnection, 2000);
-        } else {
-            throw err;
-        }
-    });
+        conn.on('error', (err) => {
+            console.error('Kesalahan koneksi:', err);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.log('Koneksi terputus. Mencoba menghubungkan kembali...');
+                handleDisconnect();
+            } else {
+                throw err;
+            }
+        });
+    }
+
+    handleDisconnect();
 
     return conn;
 }
